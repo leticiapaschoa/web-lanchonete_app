29c968e53cb9e0ab1fff68800ed5de47a1624ae5
Leticia Paschoa da Silva <leticiaps@ciandt.com>
2019-08-24 10:37:36 -0300

    Arquitetura e HomePage

---
+++ Application/ALL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ALL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
