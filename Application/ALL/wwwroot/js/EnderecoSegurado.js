3c4610e0d7d7d8873a757579aa110316a5e7741a
Leticia Paschoa da Silva <leticiaps@ciandt.com>
2019-11-07 13:06:34 -0300

    Consulta via Cep

---
+++ Application/ALL/wwwroot/js/EnderecoSegurado.js
@@ -0,0 +1,16 @@
+﻿function BuscaEndereco(cep) {
+
+    var nroCep = cep.value;
+
+    var urlApi = 'https://viacep.com.br/ws/' + nroCep + '/json/';
+
+    var viaCepHttp = new XMLHttpRequest();
+    viaCepHttp.open("GET", urlApi, false);
+    viaCepHttp.send(null);
+
+    var endereco = JSON.parse(viaCepHttp.responseText);
+
+    document.getElementById('Rua').value = endereco.logradouro;
+    document.getElementById('Bairro').value = endereco.bairro;
+    document.getElementById('Cidade').value = endereco.localidade;
+}
