18d582597cc9e7329a62b2014fa51a6597d0c0c2
Leticia Paschoa da Silva <leticiaps@ciandt.com>
2019-11-22 18:33:32 -0300

    Consulta Banco de dados projeto 2

--- Application/ALL/Controllers/ProjetoCarroController.cs
+++ Application/ALL/Controllers/ProjetoCarroController.cs
@@ -11,40 +11,54 @@ namespace WebApplication.Controllers
     {
         public IActionResult Index()
         {
-            BuscarUsuarios();
-            return View();
+            var cliente = BuscarUsuarios();
+            return View(cliente);
         }
 
-        public List<CarroUsuario> BuscarUsuarios()
+        public List<String[]> BuscarUsuarios()
         {
+            SqlConnection Conexao = new SqlConnection();
+            Conexao.ConnectionString = "server=thanos-web.database.windows.net;user id=andrelivee;password=andrelivee;Trusted_Connection=true;Database=Carros;Connection timeout=6000";
 
-            string _connection = "server=thanos-web.database.windows.net;user id=andrelivee;password=andrelivee;Trusted_Connection=true;Database=Carros;Connection timeout=6000";
-            
-                using (SqlConnection sqlcon = new SqlConnection(_connection))
-                {
-                    sqlcon.Open();
-                    SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT Nome, Email FROM Cliente", sqlcon);
-                    DataTable dtbl = new DataTable();
-                    sqlDa.Fill(dtbl);
-                    //teste.DataSource = dtbl;
-                    //teste.DataBind();
-                }
-            
+            Conexao.Open();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = Conexao;
+
+            Comando.CommandText = "SELECT * FROM cliente";
+
+            SqlDataReader Leitor = Comando.ExecuteReader();
+
+            List<String[]> clientes = new List<String[]>();
+            while (Leitor.Read())
+            {
+                String[] cliente = new String[6];
+                cliente[0] = Leitor["nome"].ToString();
+                cliente[1] = Leitor["email"].ToString();
+                
+
+                clientes.Add(cliente);
+            }
+
+            Conexao.Close();
+
+            return clientes;
 
 
 
-            string connectionString = "Server=thanos-web.database.windows.net; User Id=andrelivee; Password=@andre123;Trusted_Connection=false;Encrypt=false;TrustServerCertificate=True;Database=Carros;Connection timeout=6000";
 
-            var connection = new SqlConnection(connectionString);
-            connection.Open();
-            var command = new SqlCommand("SELECT nome, email FROM dbo.cliente", connection);
-            
-            string result = (string)command.ExecuteScalar();
-
-            connection.Close();
+            //string connString = "server=thanos-web.database.windows.net;user id=andrelivee;password=andrelivee;Trusted_Connection=true;Database=Carros;Connection timeout=6000";
 
 
-            return new List<CarroUsuario>();
+            //var connection = new SqlConnection(connString);
+            //connection.Open();
+            //var command = new SqlCommand("SELECT nome, email FROM dbo.cliente", connection);
+
+            //connection.Close();
+
+            //return command.ExecuteReader().Read();
+
+
         }
     }
 }
